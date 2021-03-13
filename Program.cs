88c1bed08b676f9ce4664d38a9b589970ec1fe83
Elmer-sv <00155620@uca.edu.sv>
2021-03-12 21:41:59 -0600

    Creating program

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GitExercise
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+
+            Console.WriteLine("Asigne un valor a la variable A");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Asigne un valor a la variable B");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"La operacion  de {a} + {b} es: {AddNumber(a,b)}");
+        }
+
+        public static int AddNumber(int a, int b)
+        {
+            return a+b;
+        }
+    }
+}
